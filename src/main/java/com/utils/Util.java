66668378e6a72ea2aad34dca73306238c3d7decb
lw <1397739779@qq.com>
2019-08-03 00:06:43 +0800

    智慧铜仁-旧版数据区域爬取-统计局

--- src/main/java/com/utils/Util.java
+++ src/main/java/com/utils/Util.java
@@ -5,6 +5,7 @@ import com.bean.Category;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class Util {
     public static void bufferedWriterWrite(BufferedWriter bw, String str) {
@@ -18,32 +19,33 @@ public class Util {
 
     /**
      * 添加AreaId后面的0
+     *
      * @param parentCode
      * @return
      */
     public static String getParentCode(String parentCode) {
         int len = parentCode.length();
-        if(len == 1) {
+        if (len == 1) {
             return parentCode + "00000000000";
-        } else if (len == 2){
+        } else if (len == 2) {
             return parentCode + "0000000000";
-        } else if (len == 3){
+        } else if (len == 3) {
             return parentCode + "000000000";
-        } else if (len == 4){
+        } else if (len == 4) {
             return parentCode + "00000000";
-        } else if (len == 5){
+        } else if (len == 5) {
             return parentCode + "0000000";
-        } else if (len == 6){
+        } else if (len == 6) {
             return parentCode + "000000";
-        } else if (len == 7){
+        } else if (len == 7) {
             return parentCode + "00000";
-        } else if (len == 8){
+        } else if (len == 8) {
             return parentCode + "0000";
-        } else if (len == 9){
+        } else if (len == 9) {
             return parentCode + "000";
-        } else if (len == 10){
+        } else if (len == 10) {
             return parentCode + "00";
-        } else if (len == 11){
+        } else if (len == 11) {
             return parentCode + "0";
         } else {
             return parentCode;
@@ -75,13 +77,14 @@ public class Util {
 
     /**
      * 去除重复Id
+     *
      * @param list
      * @return
      */
-    public static List removeDuplicateId(List<Category> list)  {
-        for  (int i = 0; i < list.size() - 1; i ++)  {
-            for  (int j = list.size() - 1; j > i; j --)  {
-                if(list.get(j).getId().equals(list.get(i).getId()))  {
+    public static List removeDuplicateId(List<Category> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = list.size() - 1; j > i; j--) {
+                if (list.get(j).getId().equals(list.get(i).getId())) {
                     list.remove(j);
                 }
             }
@@ -91,21 +94,22 @@ public class Util {
 
     /**
      * 根据父parent得到Category
-     * @param parent
+     *
+     * @param pidMark
      * @return
      */
-    public static Category getCategoryByParent(List<Category> categorys, String parent) {
+    public static Category getCategoryByParent(Map<String, Category> pidMark, String parentCode) {
         Category category1 = new Category.CategoryBuilder().build();
-        for (Category category : categorys) {
-           /* if (parent.length() == 4 && category.getCategoryCode().length() == 4) { // 市 下面的市辖区 ,县
-                 category1.setpId(category.getId());
-                 category1.setpIds(category.getpIds() + "/" + category.getId());
-                 category1.setpNames(category.getpNames());
+        for(Map.Entry<String, Category> mark : pidMark.entrySet()) {
+            /*if (parentCode.length() == 4 && mark.getValue().getCategoryCode().length() == 4) { // 市 下面的市辖区 ,县
+                 category1.setpId(mark.getValue().getId());
+                 category1.setpIds(mark.getValue().getpIds() + "/" + mark.getValue().getId());
+                 category1.setpNames(mark.getValue().getpNames());
             }*/
-            if (getParentCode(category.getCategoryCode()).equals(getParentCode(parent))) {
-                category1.setpId(category.getId());
-                category1.setpIds(category.getpIds() + "/" + category.getId());
-                category1.setpNames(category.getpNames());
+            if (getParentCode(mark.getKey()).equalsIgnoreCase(getParentCode(parentCode))) {
+                category1.setpId(mark.getValue().getId());
+                category1.setpIds(mark.getValue().getpIds() + "/" + mark.getValue().getId());
+                category1.setpNames(mark.getValue().getpNames());
             }
         }
         return category1;
